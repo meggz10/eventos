2abff74fdd3c40cbafa856dc491a093d5ecf97da
Marcos <meggsystems@gmail.com>
2021-07-26 20:04:43 -0500

    myrot-control

--- app.js
+++ app.js
@@ -360,9 +360,8 @@ const express = require('express')
 const app = express()
 
 
-const port = 3000
-
-//const port = process.env.PORT
+//const port = 3000
+const port = process.env.PORT
 //const port = process.env.
 //port;
 
